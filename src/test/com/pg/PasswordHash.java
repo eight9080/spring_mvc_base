5005de437cd6e3ac2bb54123083e2511c832aa15
eight9080 <eight9080@gmail.com>
2018-03-22 00:48:32 +0200

    spring security

---
+++ src/test/com/pg/PasswordHash.java
@@ -0,0 +1,29 @@
+package com.pg;
+
+
+import org.junit.Test;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+public class PasswordHash {
+
+
+    @Test
+    public void md5Hash(){
+        String password = "secret";
+        Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
+        String hashedPassword = passwordEncoder.encodePassword(password, null);
+        System.out.println(hashedPassword);
+    }
+
+    @Test
+    public void bCryptHash(){
+        String password = "secret";
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(password);
+        System.out.println(hashedPassword);
+    }
+
+
+}
