a6bd9b7ae274d4abe44a2123efd11af4d91f8776
eight9080 <eight9080@gmail.com>
2018-03-26 00:03:46 +0300

    spring security ldap

--- src/main/java/com/pluralsight/controller/GoalController.java
+++ src/main/java/com/pluralsight/controller/GoalController.java
@@ -26,7 +26,8 @@ public class GoalController {
 		return "addGoal";
 	}
 
-	@PreAuthorize("hasRole('ROLE_ADMIN') and hasPermission(#goal, 'createGoal')")
+//	@PreAuthorize("hasRole('ROLE_ADMIN') and hasPermission(#goal, 'createGoal')")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value = "addGoal", method = RequestMethod.POST)
 	public String updateGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
 		
